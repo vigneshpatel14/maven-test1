bc3eb184effd0196beac1b5807e746948ea3400e
vigneshpatel14 <voddamvignesh0707.com>
2025-08-17 10:36:55 +0530

    VIGNESHV0001 - Added HelloWorld Program

---
+++ src/main/java/com/example/javamavenjunithelloworld/Vignesh.java
@@ -0,0 +1,7 @@
+package com.example.javamavenjunithelloworld;
+
+public class Vignesh {
+    public static void main(String[] args) {
+        System.out.println("Hello, World from Vignesh!");
+    }
+}
